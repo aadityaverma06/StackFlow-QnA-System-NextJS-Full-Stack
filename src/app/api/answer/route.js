23f96efb6ad857b1ec7862d10672c7844ab97a6a
Aaditya Dev Verma <aditya.nomercy6@gmail.com>
2025-04-28 12:17:08 +0530

    Complete answer route

--- src/app/api/answer/route.js
+++ src/app/api/answer/route.js
@@ -1,5 +1,5 @@
 import { answerCollection, db } from "@/models/name";
-import { databases } from "@/models/server/config";
+import { databases, users } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
 import { ID } from "node-appwrite";
 
@@ -17,6 +17,35 @@ export async function POST(request) {
         authorId: authorId,
       }
     );
+    const prefs = await users.getPrefs(authorId);
+    await users.updatePrefs({
+      reputation: Number(prefs.reputation + 1),
+    });
+    return NextResponse.json({ response, status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message, status: 500 });
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const body = await request.json();
+    const { answerId } = body;
+
+    const answer = await databases.getDocument(db, answerCollection, answerId);
+
+    const response = await databases.deleteDocument(
+      db,
+      answerCollection,
+      answerId
+    );
+
+    const prefs = await users.getPrefs(answer.authorId);
+    await users.updatePrefs({
+      reputation: Number(prefs.reputation - 1),
+    });
+
+    return NextResponse.json({ response, status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message, status: 500 });
   }
