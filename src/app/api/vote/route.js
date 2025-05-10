01b28abf57c5fcbd070588aa9f0fa07f5bc98896
Aaditya Dev Verma <aditya.nomercy6@gmail.com>
2025-05-10 12:31:42 +0530

    add route

---
+++ src/app/api/vote/route.js
@@ -0,0 +1,45 @@
+import { voteCollection } from "@/models/name";
+import { databases } from "@/models/server/config";
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(request) {
+  try {
+    const { votedById, voteStatus, type, typeId } = await request.json();
+
+    const response = await databases.listDocuments(db, voteCollection, [
+      Query.equal("typeId", typeId),
+      Query.equal("type", type),
+      Query.equal("votedById", votedById),
+    ]);
+
+    if (response.documents.length > 0) {
+    }
+
+    if (response.documents[0].voteStatus !== voteStatus) {
+    }
+
+    const [upvotes, downvotes] = await Promise.all([
+      databases.listDocuments(db, voteCollection, [
+        Query.equal("typeId", typeId),
+        Query.equal("type", type),
+        Query.equal("voteStatus", "upvote"),
+        Query.equal("votedById", votedById),
+        Query.limit(1),
+      ]),
+      databases.listDocuments(db, voteCollection, [
+        Query.equal("typeId", typeId),
+        Query.equal("type", type),
+        Query.equal("voteStatus", "downvote"),
+        Query.equal("votedById", votedById),
+        Query.limit(1),
+      ]),
+    ]);
+
+    return NextResponse.json({
+      data: { document: null, voteresult: upvotes.total - downvotes.total },
+      status: 200,
+      message: "success",
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error.message, status: 500 });
+  }
+}
